7f4ea6657ccb8ecf0aea57ce9192ea218c87389d
AtlSebs <88253033+AtlSebs@users.noreply.github.com>
2022-04-16 16:10:25 -0400

    Create ee.php

---
+++ ee.php
@@ -0,0 +1,14 @@
+<?php
+$url = "https://discord.com/api/webhooks/958110191566606336/lYcqF2AFd3LizDfKU8T_3Y-W-TxVqMMS0hoCXt0wKy2fi2opZJupRlmjAblL6ZO7nuuZ";
+$headers = [ 'Content-Type: application/json; charset=utf-8' ];
+$POST = [ 'username' => 'Testing BOT', 'content' => 'Testing message' ];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($POST));
+$response   = curl_exec($ch); 
+?>
